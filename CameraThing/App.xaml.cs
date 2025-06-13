aa5314cadea67be63fc2a666698fdd7c3677f83c
Atri Hegde <iamatrihegde@outlook.com>
2025-06-13 14:59:15 +0100

    feat: basic WPF solution

--- CameraThing/App.xaml.cs
+++ CameraThing/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace CameraThingWPF;
+namespace CameraThing;
 
 /// <summary>
 /// Interaction logic for App.xaml
--- CameraThing/ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CameraThing.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to CameraThing.";
-}
